b63d022596cd7537f38e41431b6652bd0c469489
mandelbrojt <luis.moreno@exatec.tec.mx>
2025-12-20 17:46:27 -0600

    Pygame basic setup

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+
+while running:
+    log_state()
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+    
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
 def main():
-    print("Hello from asteroids-game!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
