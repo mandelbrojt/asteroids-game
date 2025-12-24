92630638d4640f9d85b1c7a337a4c1942d2c628e
mandelbrojt <luis.moreno@exatec.tec.mx>
2025-12-23 21:38:53 -0600

    Fix: Move game loop to main func and remove player instance from game loop

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-
-while running:
-    log_state()
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
-    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    player.update(dt)
-    player.draw(screen)
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt = delta time in seconds since last frame
-    # used for framerate independent physics
-    dt = clock.tick(60) / 1000
-
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while running:
+        log_state()
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("black")
+        
+        player.update(dt)
+        player.draw(screen)
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt = delta time in seconds since last frame
+        # used for framerate independent physics
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
