f321c92701cc3281019c8c3c3b292a4cd24ecf9a
mandelbrojt <luis.moreno@exatec.tec.mx>
2025-12-26 09:57:32 -0600

    Feature: Remove shot and asteroid if they collide

--- main.py
+++ main.py
@@ -54,6 +54,13 @@ def main():
                 print("Game over!")
                 sys.exit()
         
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+        
         for element in drawable:
             element.draw(screen)
         
